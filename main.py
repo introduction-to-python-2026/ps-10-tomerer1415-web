f0113e4c301129cb6b2fbbac853686aeca0e6233
Tomer erez <tomerer1415@gmail.com>
2026-01-05 22:24:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-from imageio.v2 import imwrite
-from skimage.filters import median, threshold_otsu
-from skimage.morphology import ball
-
 from image_utils import image_load, detection_edge
 
 def main():
     img = image_load("input.jpg")
-    img_clean = median(img, ball(3))
-    edges = detection_edge(img_clean)
-
-    t = threshold_otsu(edges)
-    edges_bin = edges > t
-
-    imwrite("edges.png", edges_bin.astype("uint8") * 255)
+    edges = detection_edge(img)
+    print(edges.shape)
 
 if __name__ == "__main__":
     main()
